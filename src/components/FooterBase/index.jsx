ce4439d93c65838297df895508abf1b0b06f0ab5
Daiane Alves <daianeoalves0204@gmail.com>
2023-09-30 16:19:09 -0300

    footer atualização

--- src/components/FooterBase/index.jsx
+++ src/components/FooterBase/index.jsx
@@ -3,45 +3,28 @@ import imglogo from './../../assets/imagens/logo-no-background.png'
 import 'animate.css';
 import { Button2 } from '../../assets/components/Button';
 import './index.css'
-
+import icone1 from './../../assets/imagens/facebook.png'
+import icone2 from './../../assets/imagens/instagram.png'
+import icone3 from './../../assets/imagens/linkedin.png'
 
 
 const Footer = styled.div`
 display: inline-block;
 align-items: center;
 justify-content: center;
+background-color: var(--primary);
+padding: 50px;
+height: 400px;
 
 
-
-.section{
-    padding: 5px;
-    gap: 8px;
-    margin-left: 300px;
-    margin-top: 50px;
-    background-color: var(--primary);
-    height: 400px;
-
-}
-
-.section a{
-    padding: 5px;
-    list-style: none;
-    text-decoration: none;
-    font-weight: 900;
-    color: var(--main);
-    display: flex;
-    flex-direction: column;
-
-}
 
 a:hover{
     color: #6d6868;
 }
 
 .logocolor{
-    display: flex;
-    margin-left: 300px;
-    margin-bottom: 50px;
+    margin-left: 600px;
+    margin-bottom: 20px;
 }
 
 textarea{
@@ -60,28 +43,55 @@ textarea{
 .text{
     display:flex;
     flex-direction: column;
-    margin-left: 600px;
+    justify-content: center;
+    margin-left:900px;
     margin-bottom: 20px;
 
 }
 
+h1 {
+    margin-bottom: 15px;
+    font-size: 14px;
+    width: 70%;
+    color: #2c2c2c;
+    text-align: left;
+    font-weight: 00;
+
+}
+
+.icones{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+}
+
+.icones img{
+    width: 26px;
+    height: 25px;
+    padding: 2px;
+    display: inline-block;
+    
+    
+
+}
+
+.icones img:hover{
+    transform: scale(1.2);
+}
+
 `
+
 
     function FooterBase() {
         return (
                 <Footer>
 
 
-    <div className='section'>
-
-        <a href="#">Termos de Uso</a>
-                
-        <a href="#">Central de ajuda</a>
-        
-        <a href="#"> Política de Privacidade </a>
 
         <div className='text'> 
-    <textarea placeholder='Assine a newsletter do MedData, escreva seu e-mail aqui para receber todos os dias notícias do mundo de tecnologia com a medicina!'></textarea>
+        <h1>Assine nossa newsletter e receba notícias do mundo da tecnologia com a medicina!</h1>
+    <textarea placeholder='Ex: pessoa1234@gmail.com'></textarea>
     <Button2>Enviar</Button2>
 
     
@@ -90,6 +100,12 @@ textarea{
     <img className='logocolor' src={imglogo} alt="Logo escrito MEDDATA"/>
 
 
+
+
+    <div className='icones'> 
+    <img src={icone1} alt="Logo Facebook"/>
+    <img src={icone2} alt="Logo Instagram"/>
+    <img src={icone3} alt="Logo Linkdeln"/>
     </div>
 
     </Footer>
